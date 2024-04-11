155bdc5e042799d40e22887e4f5e19b9b57c10d2
Michał Zatorski <michal_zatorski@o2.pl>
2024-04-11 22:38:05 +0200

    Final

---
+++ homesite/my_homesite.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route("/mypage/me", methods=['GET'])
+def mypage_me():
+    return render_template("site_1.html")
+
+@app.route("/mypage/contact", methods=['GET', 'POST'])
+def mypage_contact():
+    contacts = ["email: michal_zatorski@o2.pl", "telefon: 604 836 715"]
+    if request.method == 'GET':
+        return render_template("site_2.html", contacts=contacts)  
+    elif request.method == 'POST':
+       print(request.form)
+       return redirect("/mypage/contact")
